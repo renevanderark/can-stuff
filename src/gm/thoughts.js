dcf2d3f2bc4512e50f2b9b0671b105b05ceb17c3
rene <r.van.der.ark@gmail.com>
2017-08-19 13:10:54 +0200

    more stray thoughts.

--- src/gm/thoughts.js
+++ src/gm/thoughts.js
@@ -15,7 +15,9 @@ const strayThoughts = [
   ["Where did I put my keys?", "I must have left them somewhere..."],
   ["What are you waiting for?", "I'm going crazy in here!"],
   ["What were the four noble truths again?", "Suffering...", "...the origin of suffering...", "...the cessation of suffering...", "...the noble 8-fold path."],
-  ["Stuck in the wheel of becoming..."]
+  ["Stuck in the wheel of becoming..."],
+  ["3.14", "3.141", "3.1415", "3.14159", "3.141592..."],
+  ["These games always involve Pi...", "...but never 42...", "...or do they?"],
 ];
 
 const randomThought = (ary) => ary[Math.floor(Math.random() * ary.length)];
